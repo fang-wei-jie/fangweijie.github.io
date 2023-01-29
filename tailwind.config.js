5b157462de9acf75f42cdd548daacfbd05c36d90
Fang Wei Jie <fangweijie1123@gmail.com>
2023-01-29 21:43:43 +0800

    Attempt to fix Tailwind does not work on GitHub Pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: [
     "./docs/index.html",
     "./src/**/*.{html,js}",
-    "./src/**/**/*.{html,js}"
+    "./src/**/**/**/*.{html,js}"
   ],
   theme: {
     extend: {},
