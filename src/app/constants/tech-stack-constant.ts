dca78bce6b40c806658a60a5edfd482fa830a8eb
Wei Jie <weijie@bukku.my>
2023-12-02 17:10:36 +0800

    Update assets

--- src/app/constants/tech-stack-constant.ts
+++ src/app/constants/tech-stack-constant.ts
@@ -1,13 +1,49 @@
 import {ISkill} from "../interface/i-skill";
 
 export class techStackConstant {
-  public static angular: ISkill =  { link: "/assets/skills/angular.png", name: "Angular" }
-  public static angularJs: ISkill =  { link: "/assets/skills/angularJS.png", name: "AngularJS" }
-  public static springBoot: ISkill =  { link: "/assets/skills/spring-boot.png", name: "Spring Boot" }
-  public static tailwind: ISkill =  { link: "/assets/skills/tailwind-css.png", name: "Tailwind CSS" }
-  public static ngZorro: ISkill =  { link: "/assets/skills/ng-zorro.png", name: "NG-ZORRO" }
-  public static laravel: ISkill = { link: "/assets/skills/laravel.png", name: "Laravel" }
-  public static bootstrap5: ISkill = { link: "/assets/skills/bootstrap-5.png", name: "Bootstrap 5" }
-  public static bootstrap3: ISkill = { link: "/assets/skills/bootstrap-3.png", name: "Bootstrap 3" }
-  public static laravelLivewire: ISkill = { link: "/assets/skills/laravel-livewire.png", name: "Laravel Livewire" }
+  public static angular: ISkill = {
+      link: "/assets/frameworks/angular.svg",
+      name: "Angular"
+  }
+
+  public static angularJs: ISkill = {
+      link: "/assets/skills/angularJS.png",
+      name: "AngularJS"
+  }
+
+  public static springBoot: ISkill = {
+      link: "/assets/skills/spring-boot.png",
+      name: "Spring Boot"
+  }
+
+  public static tailwind: ISkill = {
+      link: "/assets/skills/tailwind-css.png",
+      name: "Tailwind CSS"
+  }
+
+  public static ngZorro: ISkill = {
+      link: "/assets/skills/ng-zorro.png",
+      name: "NG-ZORRO"
+  }
+
+  public static laravel: ISkill ={
+      link: "https://laravel.com/img/logomark.min.svg",
+      name: "Laravel"
+  }
+
+  public static bootstrap5: ISkill ={
+      link: "https://getbootstrap.com/docs/5.0/assets/brand/bootstrap-logo.svg",
+      name: "Bootstrap 5"
+  }
+
+  public static bootstrap3: ISkill ={
+      link: "/assets/skills/bootstrap-3.png",
+      name: "Bootstrap 3"
+  }
+
+  public static laravelLivewire: ISkill ={
+      link: "/assets/skills/laravel-livewire.png",
+      name: "Laravel Livewire"
+  }
+
 }
