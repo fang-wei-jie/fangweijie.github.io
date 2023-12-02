526ce5181f261e27af43e2fc19d57a5d8eb2bd36
Wei Jie <weijie@bukku.my>
2023-12-02 16:39:08 +0800

    Update list of tools

--- src/app/constants/tool-constant.ts
+++ src/app/constants/tool-constant.ts
@@ -1,7 +1,23 @@
 import {ISkill} from "../interface/i-skill";
 
 export class toolConstant {
-  public static idea: ISkill = { link: "/assets/skills/IDEA.png", name: "IntelliJ IDEA" }
-  public static vscode: ISkill = { link: "/assets/skills/vscode.png", name: "VS Code" }
-  public static figma: ISkill = { link: "/assets/skills/figma.png", name: "Figma" }
+    public static vscode: ISkill = {
+        link: "/assets/skills/vscode.png",
+        name: "VS Code"
+    }
+
+    public static figma: ISkill = {
+        link: "/assets/skills/figma.png",
+        name: "Figma"
+    }
+
+    public static idea: ISkill = {
+        link: "https://resources.jetbrains.com/storage/products/company/brand/logos/IntelliJ_IDEA_icon.svg",
+        name: "IntelliJ IDEA"
+    }
+
+    public static phpStorm: ISkill = {
+        link: "https://resources.jetbrains.com/storage/products/company/brand/logos/PhpStorm_icon.svg",
+        name: "PhpStorm"
+    }
 }
